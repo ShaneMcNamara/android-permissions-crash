12f3878e182b3ae42f3a73be6884c94b132e2cc3
ShaneMcNamara <shanemcnamara11@gmail.com>
2021-03-18 12:01:18 -0700

    Simplified permissions crash

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Button} from 'react-native';
-import * as Location from 'expo-location';
+import React, { useEffect } from 'react';
+import { StyleSheet, View} from 'react-native';;
 import * as Permissions from 'expo-permissions';
 
 import Camera from './Camera';
 
-const getPerms = async () => {
-  try {
-    await Permissions.askAsync(Permissions.LOCATION);
-  } catch (err) {
-    console.log('Failed to get Location Perms', err);
-  }
-};
-
 export default function App() {
-  const [showCamera,setShowCamera] = useState(false);
   useEffect(() => {
-    getPerms();
+    Permissions.askAsync(Permissions.LOCATION);
   },[])
   return (
     <View style={styles.container}>
-      {!showCamera && <Button
-        title='Open Camera'
-        onPress={() => setShowCamera(true)}
-      />}
-      <Camera visible={showCamera} onClose={() => setShowCamera(false)}/>
+      <Camera/>
     </View>
   );
 }
--- Camera.js
+++ Camera.js
@@ -1,40 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text,View } from 'react-native';
+import { useEffect } from 'react';
 import { Camera } from 'expo-camera';
 
-export default ({ visible, onClose }) => {
-  const [hasPerms,setHasPerms] = useState(false);
+export default () => {
   useEffect(() => {
-    const init = async () => {
-      try {
-        const { status:cameraPerms } = await Camera.requestPermissionsAsync();
-        setHasPerms(cameraPerms === 'granted')
-      } catch(err) {
-        console.log('Failed to get Camera Perms', err);
-      } 
-    };
-    init();
+    Camera.requestPermissionsAsync();
   },[]);
-  const hideButton = (
-    <Button
-      title='Hide Camera'
-      onPress={onClose} 
-    />
-  );
-  if(!visible) return null;
-  return (
-    hasPerms ? <Camera style={StyleSheet.absoluteFill}>
-      {hideButton}
-    </Camera> : (
-      <View style={[StyleSheet.absoluteFill,{ justifyContent:'center', flexDirection:'row'}]}>
-        <View style={{ justifyContent:'center'}}>
-          <Text style={{ width: '100%', marginBottom: 10 }}>Access to camera is denied</Text>
-          <View style={{ width:'100%', height: 40,}}>
-            {hideButton}
-          </View>
-
-        </View>
-      </View>
-    )
-  );
+  return null;
 }
